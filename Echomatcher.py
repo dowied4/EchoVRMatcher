901267129ceed8b5eebc91b454693fc598f3f43c
David <dowied4@mymacewan.ca>
2019-01-22 22:36:13 -0700

    Fetches game id and displays to user. Added browse button to search for the executables path

--- Echomatcher.py
+++ Echomatcher.py
@@ -11,13 +11,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.setEnabled(True)
-        MainWindow.resize(800, 600)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setFixedSize(800,600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.joinButton = QtWidgets.QPushButton(self.centralwidget)
@@ -26,10 +25,10 @@ class Ui_MainWindow(object):
         self.fetchButton = QtWidgets.QPushButton(self.centralwidget)
         self.fetchButton.setGeometry(QtCore.QRect(530, 310, 111, 41))
         self.fetchButton.setObjectName("fetchButton")
-        self.matchEntry = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.matchEntry = QtWidgets.QLineEdit(self.centralwidget)
         self.matchEntry.setGeometry(QtCore.QRect(220, 310, 311, 41))
         self.matchEntry.setObjectName("matchEntry")
-        self.browseEntry = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.browseEntry = QtWidgets.QLineEdit(self.centralwidget)
         self.browseEntry.setGeometry(QtCore.QRect(220, 200, 311, 41))
         self.browseEntry.setObjectName("browseEntry")
         self.label = QtWidgets.QLabel(self.centralwidget)
@@ -46,9 +45,9 @@ class Ui_MainWindow(object):
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.fetchButton.clicked.connect(self.fetchid)
-
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.browseButton.clicked.connect(self.set_path)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -58,11 +57,29 @@ class Ui_MainWindow(object):
         self.label.setText(_translate("MainWindow", "EchoVR Location"))
         self.browseButton.setText(_translate("MainWindow", "Browse"))
         self.label_2.setText(_translate("MainWindow", "Match ID"))
+        try:
+            file = open("pathFile.txt","r")
+            self.path = file.read()
+            if(self.path==""):
+                self.path = "C:\\Program Files\\Oculus\\Software\\Software\\ready-at-dawn-echo-arena\\bin\\win7\\echovr.exe"
+        except:
+            self.path = "C:\\Program Files\\Oculus\\Software\\Software\\ready-at-dawn-echo-arena\\bin\\win7\\echovr.exe"
+        self.browseEntry.setText(_translate("MainWindow",self.path))
 
     def fetchid(self):
         game_state = echovr_api.fetch_state()
-        matchEntry.setText(game_state.sessionid)
-        return
+        print(game_state.sessionid)
+        self.matchEntry.setText(game_state.sessionid)
+
+    def set_path(self):
+        fileName, _= QtWidgets.QFileDialog.getOpenFileName(None, "Select Match ID file","", "exe Files (*.exe)")
+        if fileName:
+            self.browseEntry.setText(fileName)
+            pathFile = open("pathFile.txt", "w")
+            pathFile.write(fileName)
+            pathFile.close()
+
+
 
 if __name__ == "__main__":
     import sys
