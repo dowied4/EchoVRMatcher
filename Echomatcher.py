a20726a1706f61f147e31ce44c9ce4c380378e59
dowie <dowied4@mymacewan.ca>
2020-07-08 16:23:11 -0600

    Removes radial buttons and adds ability to join specific teams.
    Game api was updated with the capability of joining specific teams. Also
    recompiling project as api runs on a different port.

--- Echomatcher.py
+++ Echomatcher.py
@@ -25,7 +25,7 @@ class Ui_MainWindow(object):
         self.label_2.setGeometry(QtCore.QRect(160, 280, 140, 16))
         self.label_2.setObjectName("label_2")
         self.joinLabel = QtWidgets.QLabel(self.centralwidget)
-        self.joinLabel.setGeometry(QtCore.QRect(580,290,100,20))
+        self.joinLabel.setGeometry(QtCore.QRect(515,280,100,20))
         self.joinLabel.setObjectName("joinLabel")
         self.matchIDLabel = QtWidgets.QLabel(self.centralwidget)
         self.matchIDLabel.setGeometry(QtCore.QRect(100,100,180,50))
@@ -54,21 +54,21 @@ class Ui_MainWindow(object):
         font.setWeight(80)
         self.label.setFont(font)
         self.label_2.setFont(font)
-        self.joinLabel.setFont(font)
+        self.joinLabel.setFont(font)    
         self.matchIDLabel.setFont(font)
 
         #spectate radio button
-        self.spectateRadio = QtWidgets.QRadioButton(self.centralwidget)
-        self.spectateRadio.setGeometry(QtCore.QRect(500, 320, 120, 20))
-        self.spectateRadio.setObjectName("spectateRadio")
-        self.spectateRadio.setFont(font)
+        # self.spectateRadio = QtWidgets.QRadioButton(self.centralwidget)
+        # self.spectateRadio.setGeometry(QtCore.QRect(500, 320, 120, 20))
+        # self.spectateRadio.setObjectName("spectateRadio")
+        # self.spectateRadio.setFont(font)
 
-        #player radio button
-        self.playerRadio = QtWidgets.QRadioButton(self.centralwidget)
-        self.playerRadio.setGeometry(QtCore.QRect(650, 320, 120, 20))
-        self.playerRadio.setObjectName("playerRadio")
-        self.playerRadio.setFont(font)
-        self.userType = "P" #monitor if user wants to be a spectator or a player
+        # player radio button
+        # self.playerRadio = QtWidgets.QRadioButton(self.centralwidget)
+        # self.playerRadio.setGeometry(QtCore.QRect(650, 320, 120, 20))
+        # self.playerRadio.setObjectName("playerRadio")
+        # self.playerRadio.setFont(font)
+        # self.userType = "P" #monitor if user wants to be a spectator or a player
 
 
         #join button
@@ -82,15 +82,39 @@ class Ui_MainWindow(object):
         self.launchButton.setObjectName("launchButton")
         self.launchButton.setFont(joinFont)
         self.launchButton.setToolTip('Launches game')
-
         joinFont.setPointSize(10)
+
         #fetch button
-        self.joinButton = QtWidgets.QPushButton(self.centralwidget)
-        self.joinButton.setGeometry(QtCore.QRect(360, 310, 111, 41))
-        self.joinButton.setObjectName("joinButton")
-        self.joinButton.setFont(font)
-        self.joinButton.setToolTip('Joins match from id')
-        self.joinButton.setFont(joinFont)
+        # self.joinButton = QtWidgets.QPushButton(self.centralwidget)
+        # self.joinButton.setGeometry(QtCore.QRect(360, 310, 111, 41))
+        # self.joinButton.setObjectName("joinButton")
+        # self.joinButton.setFont(font)
+        # self.joinButton.setToolTip('Joins match from id')
+        # self.joinButton.setFont(joinFont)
+
+        # Blue team
+        self.joinBlue = QtWidgets.QPushButton(self.centralwidget)
+        self.joinBlue.setGeometry(QtCore.QRect(375,310,85,41))
+        self.joinBlue.setObjectName("joinBlue")
+        self.joinBlue.setFont(font)
+        self.joinBlue.setToolTip("Joins Blue Team from id")
+        self.joinBlue.setFont(joinFont)
+
+        # Orange team
+        self.joinOrange = QtWidgets.QPushButton(self.centralwidget)
+        self.joinOrange.setGeometry(QtCore.QRect(615,310,85,41))
+        self.joinOrange.setObjectName("joinOrange")
+        self.joinOrange.setFont(font)
+        self.joinOrange.setToolTip("Joins Orange Team from id")
+        self.joinOrange.setFont(joinFont)
+
+        # Spectator
+        self.joinSpec = QtWidgets.QPushButton(self.centralwidget)
+        self.joinSpec.setGeometry(QtCore.QRect(475,310,125,41))
+        self.joinSpec.setObjectName("joinSpec")
+        self.joinSpec.setFont(font)
+        self.joinSpec.setToolTip("Joins Spectator from id")
+        self.joinSpec.setFont(joinFont)
 
         #clipboard button
         self.clipButton = QtWidgets.QPushButton(self.centralwidget)
@@ -122,11 +146,14 @@ class Ui_MainWindow(object):
         #method calls
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        self.joinButton.clicked.connect(self.join_match)
+        # self.joinButton.clicked.connect(self.join_match)
         self.browseButton.clicked.connect(self.set_path)
         self.launchButton.clicked.connect(self.launch_game)
-        self.spectateRadio.toggled.connect(self.to_spec)
-        self.playerRadio.toggled.connect(self.to_play)
+        # self.spectateRadio.toggled.connect(self.to_spec)
+        # self.playerRadio.toggled.connect(self.to_play)
+        self.joinBlue.clicked.connect(self.join_blue)
+        self.joinOrange.clicked.connect(self.join_orange)
+        self.joinSpec.clicked.connect(self.join_spectator)
         self.clipButton.clicked.connect(self.fetch_id)
         self.emsg = QtWidgets.QErrorMessage()
         self.emsg.setWindowModality(QtCore.Qt.WindowModal)
@@ -136,16 +163,19 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "EchoVR Matcher"))
         self.launchButton.setText(_translate("MainWindow", "LAUNCH GAME"))
-        self.joinButton.setText(_translate("MainWindow", "Join Match"))
+        # self.joinButton.setText(_translate("MainWindow", "Join Match"))
+        self.joinBlue.setText(_translate("MainWindow", "Blue"))
+        self.joinOrange.setText(_translate("MainWindow","Orange"))
+        self.joinSpec.setText(_translate("MainWindow", "Spectator"))
         self.label.setText(_translate("MainWindow", "EchoVR Location"))
         self.browseButton.setText(_translate("MainWindow", "Browse"))
         self.label_2.setText(_translate("MainWindow", "Target Match ID"))
         self.joinLabel.setText(_translate("MainWindow", "Join As:"))
         self.matchIDLabel.setText(_translate("MainWindow", "Current Match ID: "))
         self.IDLabel.setText(_translate("MainWindow", "ID Unavailable(please launch through EchoVR Matcher)"))
-        self.spectateRadio.setText(_translate("MainWindow", "Spectator"))
-        self.playerRadio.setText(_translate("MainWindow", "Player"))
-        self.playerRadio.toggle()
+        # self.spectateRadio.setText(_translate("MainWindow", "Spectator"))
+        # self.playerRadio.setText(_translate("MainWindow", "Player"))
+        # self.playerRadio.toggle()
 
         #will initialise the default path if one does not exist
         try:
@@ -161,12 +191,12 @@ class Ui_MainWindow(object):
        self.emsg.showMessage("EchoVR.exe could not be found. Please enter a valid path!")
         
     #change userType to spectator
-    def to_spec(self):
-        self.userType = "S"
+    # def to_spec(self):
+    #     self.userType = "S"
 
     #change userType to Spectator
-    def to_play(self):
-        self.userType = "P"
+    # def to_play(self):
+    #     self.userType = "P"
 
     def poll_id(self):
         try:
@@ -200,18 +230,17 @@ class Ui_MainWindow(object):
         print(self.path)
 
     #gets command based off state of app
-    def get_command(self):
-        if self.userType == "P":
-            if self.matchEntry.text() == "":
-                command = "\"" + self.path + "\""
-            else:
-                command = "\"" + self.path + "\""  + " -lobbyid " + self.matchEntry.text()
+    def get_command(self,team):
+        if self.matchEntry.text() != "":
+            if team == "b":
+                return "\"" + self.path + "\"" + " -lobbyid " + self.matchEntry.text() + "-lobbyteam " + "0"
+
+            if team == "o":
+                return "\"" + self.path + "\"" + " -lobbyid " + self.matchEntry.text() + "-lobbyteam " + "1"
+            if team =="s":
+                return "\"" + self.path + "\"" + " -lobbyid " + self.matchEntry.text() + "-lobbyteam " + "2"
         else:
-            if self.matchEntry.text() == "":
-                command = "\"" + self.path + "\"" + "-spectatorstream"
-            else:
-                command = "\"" + self.path + "\""  + " -spectatorstream -lobbyid " + self.matchEntry.text()
-        return command
+            self.showError()
 
     def launch_game(self):
         self.path = self.browseEntry.text()
@@ -226,18 +255,31 @@ class Ui_MainWindow(object):
             self.showError()
 
     #joins match from a passed sessionid
-    def join_match(self):
+    def join_match(self, com):
         self.path = self.browseEntry.text()
-        command = self.get_command()
         if "echovr.exe" in self.path:
             try:
                 subprocess.call('taskkill /IM echovr.exe')
-                subprocess.Popen(command)
+                subprocess.Popen(com)
             except:
                 self.showError()
         else:
             self.showError()
 
+    def join_blue(self):
+        self.path = self.browseEntry.text()
+        command = self.get_command("b")
+        self.join_match(command)
+
+    def join_orange(self):
+        self.path = self.browseEntry.text()
+        command = self.get_command("o")
+        self.join_match(command)
+
+    def join_spectator(self):
+        self.path = self.browseEntry.text()
+        command = self.get_command("s")
+        self.join_match(command)
 
 
 if __name__ == "__main__":
